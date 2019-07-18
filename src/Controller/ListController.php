b6e128019b4de7f3bf6ce901f20ff4c4f6c5aa01
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-07-18 20:56:05 +0200

    implements methods

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,24 +2,73 @@
 
 namespace App\Controller;
 
+use App\Entity\TaskList;
+use App\Repository\TaskListRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use http\Env\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcher;
+use Symfony\Component\HttpFoundation\Response;
 
 class ListController extends AbstractFOSRestController
 {
+
+    /**
+     * @var TaskListRepository
+     */
+    private $taskListRepository;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    public function __construct(
+        TaskListRepository $taskListRepository,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->taskListRepository = $taskListRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return \App\Entity\TaskList[]
+     */
     public function getListsAction()
     {
-        
+        $data = $this->taskListRepository->findAll();
+        return $this->view($data, Response::HTTP_OK);
     }
 
     public function getListAction(int $id)
     {
+        $data = $this->taskListRepository->findOneBy(['id'=>$id]);
 
+        return $this->view($data, Response::HTTP_OK);
     }
 
-    public function postListsAction()
+    /**
+     * @Rest\RequestParam(name="title", description="title of list")
+     * @param ParamFetcher $paramFetcher
+     */
+    public function postListsAction(ParamFetcher $paramFetcher)
     {
-        
+        $title = $paramFetcher->get('title');
+
+        if($title){
+            $list = new TaskList();
+
+            $list->setTitle($title);
+
+            $this->entityManager->persist($list);
+            $this->entityManager->flush();
+
+            return $this->view($list, Response::HTTP_OK);
+        }
+
+        return $this->view(['list'=>'This can not be null'], Response::HTTP_BAD_REQUEST);
+
     }
 
     public function getListTasksAction(int $id)
@@ -29,13 +78,13 @@ class ListController extends AbstractFOSRestController
 
     public function putListsAction()
     {
-        
+
     }
 
     public function patchListsAction(int $id)
     {
-        
+
     }
 
-    
+
 }
